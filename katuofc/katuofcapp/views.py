76e48ff30d72b19b481ff7bb508f9033c3a35712
josejuniortk <josemjuniortk@gmail.com>
2022-08-05 03:34:06 +0000

    Update

---
+++ katuofc/katuofcapp/views.py
@@ -0,0 +1,16 @@
+from html.entities import name2codepoint
+from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
+import os
+import json
+from katuofc.katuofcapp import core
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def facilitamei_index(request):
+    return render(request, 'facilitamei_index.html')
