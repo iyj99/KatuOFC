03e4e0c9113794d44ac4661a5347655d203ed210
josejuniortk <josemjuniortk@gmail.com>
2022-10-07 18:13:21 +0000

    graph foo

--- katuofc/katuofcapp/views.py
+++ katuofc/katuofcapp/views.py
@@ -21,7 +21,8 @@ def facilitamei_dashboard(request):
 
 
 def facilitamei_sobre(request):
-    return render(request, 'facilitamei_sobre.html', {'variavel1': {'Janeiro': 30, 'Fevereiro': 20, 'Dezembro': 90}})
+    variavel1 = "testando 1 2 3"
+    return render(request, 'facilitamei_sobre.html', {'variavel1': variavel1})
 
 
 def pp(request):
