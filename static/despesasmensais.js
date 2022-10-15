92a9781fa796cabdfe1df2c8ce726f5329906cf4
Leandro Hyun <58566994+iyj99@users.noreply.github.com>
2022-10-15 19:22:40 -0300

    Update despesasmensais.js

--- static/despesasmensais.js
+++ static/despesasmensais.js
@@ -1,26 +1,33 @@
+function valor_meses(arquivo){
+  var labels_meses = Object.keys(arquivo)
+  return labels_meses
+}
+
+function valor(arquivo){
+  var dados_valores = Object.values(arquivo)
+  return dados_valores
+}
+
+let meses = valor_meses(variavel1)
+
+let valores = valor(variavel1)
+
+console.log(meses)
+
+console.log(valores)
+
+//Código do Gráfico
+
 const ctx = document.getElementById("myChart")
 
-const labels = [
-    'Agosto',
-    'Setembro',
-    'Outubro',
-    'Dezembro',
-    'Janeiro',
-    'Fevereiro',
-    'Março',
-    'Abril',
-    'Maio',
-    'Junho',
-    'Julho',
-  
-]
+const labels = meses
 
 const data = {
     labels,
     datasets:[{
         backgroundColor: 'rgb(2, 46, 156)',
         borderColor: 'rgb(21, 60, 234)',
-        data: [2013, 2500, 2301, 2400, 2532, 5212, 2900, 3150, 2403, 2000, 4000, 3800],
+        data: valores,
         label: "Despesas por Mensais"
     }]
     
