188b41a5220b7934e853958bea13558030cd5d0b
Leandro Hyun <58566994+iyj99@users.noreply.github.com>
2022-10-28 15:24:52 -0300

    Update lucroxdespesas.js

--- static/lucroxdespesas.js
+++ static/lucroxdespesas.js
@@ -1,3 +1,52 @@
+function valor(arquivo){
+  var dados_valores = Object.values(arquivo)
+  return dados_valores
+}
+
+let valoresLucroXDespesas = valor(variavel1)
+console.log(valoresLucroXDespesas)
+
+//Código do Gráfico
+
+const ctx = document.getElementById("myChart")
+
+const labels = [
+  
+]
+
+const data = {
+    labels: [
+        'Lucro',
+        'Despesas',
+      ],
+    datasets:[{
+       //  borderColor: 'rgb(255, 99, 132)',
+        data: valoresLucroXDespesas,
+        backgroundColor: [
+            'rgb(28, 113, 209',
+            'rgb(241, 32, 32)',
+          ],
+          hoverOffset: 4,
+        label: "Progressão de vendas"
+    }]
+}
+
+const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        responsive: true
+    }
+  };
+
+  const myChart = new Chart(ctx, config)
+  
+  
+  
+  
+  
+  
+/*
 const ctx = document.getElementById("myChart")
 
 const labels = [
@@ -30,3 +79,5 @@ const config = {
   };
 
   const myChart = new Chart(ctx, config)
+  
+  */
