26d76300c1c6ea22e6669ef4b09b7e16d8152ff7
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2022-11-24 16:25:32 -0300

    Mudanças na página de dashboard - 24/11/22

--- static/assets/src_page/js_page/script.js
+++ static/assets/src_page/js_page/script.js
@@ -2,7 +2,10 @@ let opcoes_calendario = document.querySelector('.opcoes_calendario');
 let caixa_upload = document.querySelector('.caixa_upload');
 let container_opcoes = document.querySelector('.container_opcoes');
 
-let body = document.querySelector('body');
+let visibility_bttn_desktop = document.querySelector('.visibility_bttn_desktop');
+let visibility_bttn_mobile = document.querySelector('.visibility_bttn_mobile');
+let visibility_off_icon = "../static/assets/src_page/images_page/visibility_off_icon.svg";
+let visibility_icon = "../static/assets/src_page/images_page/visibility_icon.svg";
 
 function abrir_calendario(){
     opcoes_calendario.classList.toggle('display_none');
@@ -42,12 +45,16 @@ function esconder_dados(){
             lista_valores[i].style.backgroundColor = 'black';
             lista_valores[i].style.height = '.8rem';
         };
+        visibility_bttn_desktop.src = visibility_off_icon;
+        visibility_bttn_mobile.src = visibility_off_icon;
     }
     else{
         for(i=0; i < lista_valores.length; i++){
             lista_valores[i].style.backgroundColor = 'white';
             lista_valores[i].style.height = 'initial';
         };
+        visibility_bttn_desktop.src = visibility_icon;
+        visibility_bttn_mobile.src = visibility_icon;
     };
 };
 
