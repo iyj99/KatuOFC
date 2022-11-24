d3398edf63a3bdbba0a90ebff77c3e5dd41c26b4
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2022-11-24 12:18:44 -0300

    Correção de bugs no dashboard
    - Corrigido bug ao abrir caixa de upload após o do calendário.
    - Corrigido bug ao abrir caixa de upload e do calendário na versão mobile.

--- static/assets/src_page/js_page/script.js
+++ static/assets/src_page/js_page/script.js
@@ -2,6 +2,8 @@ let opcoes_calendario = document.querySelector('.opcoes_calendario');
 let caixa_upload = document.querySelector('.caixa_upload');
 let container_opcoes = document.querySelector('.container_opcoes');
 
+let body = document.querySelector('body');
+
 function abrir_calendario(){
     opcoes_calendario.classList.toggle('display_none');
     caixa_upload.classList.add('display_none');
@@ -9,9 +11,15 @@ function abrir_calendario(){
 };
 
 function abrir_caixa_upload(){
-    caixa_upload.classList.toggle('display_none');
+    if(caixa_upload.classList.contains('display_none')){
+        caixa_upload.classList.remove('display_none');
+        container_opcoes.classList.remove('display_none');
+    }
+    else{
+        caixa_upload.classList.add('display_none');
+        container_opcoes.classList.add('display_none');
+    };
     opcoes_calendario.classList.add('display_none');
-    container_opcoes.classList.toggle('display_none');
 };
 
 function voltar_para_inicio(){
